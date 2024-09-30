6721990aa1c75b4a18e07351a414922fe0f16a75
Matheus Cardoso <omatheusncardoso@gmail.com>
2024-09-30 17:01:20 -0400

    Update app.py
    Implementação das funções:
    
    -Imprimir destino
    -Apagar destino
    
    E alterações e pequenas formatações automáticas na gravação do nome da cidade.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import json
 import os
 
+#Criando as cidades
 class Cidades:
     def __init__(self, nome):
         self.nome = nome
@@ -23,15 +24,15 @@ class Cidades:
                 for adj in self.adjacentes
             ]
         }
-
+#Criando as conexoes para fazer um grafo
 class Adjacentes:
     def __init__(self,nome, custo):
         self.nome = nome
         self.custo = custo
-
+#Cadastrando a cidade
 def cadastrarCidade():
     nomeCid = input("Digite o nome da cidade a ser cadastrada: ")
-    cidade = Cidades(nomeCid)
+    cidade = Cidades(nomeCid.capitalize())
     os.system('cls')
     print("CADASTRE AS CIDADES VIZINHAS DE {}".format(nomeCid))
     
@@ -48,19 +49,39 @@ def cadastrarCidade():
             os.system('cls')
 
     cidade.salvarCidade()
+#Apagar cidade
+def apagarCidade(cidade): 
+    if len(cidadesDict) == 0:
+        print("Sua lista de destinos está vazia!")
+    else:
+        cidade = cidade.capitalize() 
+        valorRemovido = cidadesDict.pop(cidade, None)
+        if valorRemovido == None:
+            print('Destino não encontrado, por favor tente novamente!')
+        else:
+            print('{} removido com sucesso!'.format(cidade))
+#Imprimir Cidade
+def imprimirCidades():
+    if len(cidadesDict) == 0:
+        print("Sua lista de destinos está vázia!")
+    else:
+        for i in cidadesDict:
+            print("Cidade: {}".format(i))
+            for adj in cidadesDict[i]["adjacentes"]:
+                print("Conexão: {} á {} Km de distância.".format(adj["nome"], adj["custo"]))
+            print("=-="*20)
 
-def apagarCidade(cidade):
-    print(cidade) #EM CONSTRUÇAO
 
+#Main para controlar o código
 def main():
     controlador = True
     while controlador == True:
-        print("=-="*10)
-        print("1. Cadastrar cidade")
-        print("2. Apagar cidade") #Em construção
-        print("3. Imprimir cidades") #Em construção
+        print("=-="*20)
+        print("1. Cadastrar Destino")
+        print("2. Apagar Destino") 
+        print("3. Imprimir Destinos") 
         print("4. Sair")
-        print("=-="*10)
+        print("=-="*20)
         opcao = int(input("Escolha uma opção: "))
         os.system('cls')
 
@@ -69,17 +90,19 @@ def main():
         elif opcao == 2:
             apagar = input("Digite a cidade a ser apagada: ")
             apagarCidade(apagar)
+        elif opcao == 3:
+            imprimirCidades()
         elif opcao == 4:
             controlador = False
-
+#Abrindo o arquivo Json para salvar o código
 nomeArquivo = 'cidades.json'
 if os.path.exists(nomeArquivo):
     with open(nomeArquivo, 'r') as f:
         cidadesDict = json.load(f)
 else:
     cidadesDict= {}
-
+#Chamando a main para iniciar a aplicacao
 main()
-
+#salvando as altercoes no dicionario
 with open(nomeArquivo, 'w') as f:
     json.dump(cidadesDict, f, indent=4)
